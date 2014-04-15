56ef3bb09cd4534213b98faeacb2b805b0a50553
Michael T. Draper <mtdraper@iupui.edu>
2014-04-15 19:58:16 -0400

    Scaffold Fixing

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
-  validates :category_id, presence: true
   has_many :tasks
   belongs_to :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :category
+  validates :category_id, presence: true
 end
